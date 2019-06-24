5c05e1a6ae94783363757264ff23eb15f9bab934
Baz <boruch11@gmail.com>
2019-06-24 14:04:38 +1000

    Dont print out key values
    Signed-off-by: Baz <boruch11@gmail.com>

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+set +e +x
+
+cd ./BigQueryPublisher
+echo ${GoogleServiceAccountCredentials} >> key.json
+unset GoogleServiceAccountCredentials
+
 set -ex
 
-echo ${GoogleServiceAccountCredentials} >> key.json
 
 gcloud auth activate-service-account concourse-deployment@selfhydro-197504.iam.gserviceaccount.com --key-file key.json --project selfhydro
 gcloud functions deploy TransferStateToBigQuery --runtime go111 --env-vars-file .env.yaml --trigger-topic projects/selfhydro-197504/topics/telemetry-topic
